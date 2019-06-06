932a7cfc0f2d79952d887d2e3787388e6bb52ed6
Joan Andrés <andres6936@live.com>
2019-06-05 22:31:36 -0500

    Add EItemType.hpp

---
+++ Source/Include/EItemType.hpp
@@ -0,0 +1,14 @@
+#ifndef SYSTEMINVENTORY_EITEMTYPE_HPP
+#define SYSTEMINVENTORY_EITEMTYPE_HPP
+
+enum EItemType
+{
+    /*
+     * Any object of this type must be treated as a null object.
+     */
+            NONE = 0,
+
+    SHIELD
+};
+
+#endif //SYSTEMINVENTORY_EITEMTYPE_HPP
